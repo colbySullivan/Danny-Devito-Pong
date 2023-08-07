712992c2f954e3999c832d85f9d161435d69de92
Colby Sullivan <88957296+colbySullivan@users.noreply.github.com>
2023-08-07 15:00:55 -0500

    Rename header

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,4 @@
-#include "Game.h"
+#include "Game.hpp"
 #include <string> 
 
 void Game::initVariables(){
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Game.h"
+#include "Game.hpp"
 #include "Game.cpp"
 
 int main(){
