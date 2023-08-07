9efa6585b7cc9e40d82380b16e655905b958710e
Colby Sullivan <88957296+colbySullivan@users.noreply.github.com>
2023-08-07 15:14:27 -0500

    Documentation

--- Game.hpp
+++ Game.hpp
@@ -17,85 +17,84 @@
 #include <SFML/Main.hpp>
 
 /*
-	Class that acts as the game engine.
-	Wrapper class.
-*/
-
-class Game{
+ * Class that acts as the game engine.
+ * This is a wrapper class for the game functionality.
+ */
+class Game {
 private:
-    // Variables
-    float pi;
-    float gameWidth;
-    float gameHeight;
-    sf::Vector2f paddleSize;
-    float ballRadius;
-    sf::Clock AITimer;
-    sf::Time AITime;
-    float paddleSpeed;
-    float basePaddleSpeed;
-    float rightPaddleSpeed;
-    float baseBallSpeed;
-    float ballSpeed;
-    float ballAngle;
-    float dannyAngle;
-    sf::Clock clock;
-    sf::Clock dannyClock;
-    int dannySpeed;
+    // Constants and Configuration Variables
+    float pi;                      // The value of pi.
+    float gameWidth;               // Width of the game window.
+    float gameHeight;              // Height of the game window.
+    sf::Vector2f paddleSize;       // Size of the paddles.
+    float ballRadius;              // Radius of the ball.
+    sf::Clock AITimer;             // Timer for controlling AI behavior.
+    sf::Time AITime;               // Time interval for AI updates.
+    float paddleSpeed;             // Speed of the player's paddle.
+    float basePaddleSpeed;         // Base speed of the paddles.
+    float rightPaddleSpeed;        // Speed of the AI's paddle on the right side.
+    float baseBallSpeed;           // Base speed of the ball.
+    float ballSpeed;               // Current speed of the ball.
+    float ballAngle;               // Angle at which the ball moves.
+    float dannyAngle;              // Angle for AI player's movement.
+    sf::Clock clock;               // Clock for tracking game time.
+    sf::Clock dannyClock;          // Clock for controlling AI's movement.
+    int dannySpeed;                // Speed of the AI player's movement.
 
-    //Window setup
-    sf::RenderWindow* window;
-    sf::Sprite loadscreen;
-    sf::Texture dannyTexture;
-    sf::IntRect dannySprite;
-    sf::Sprite sprite;
+    // Window Setup
+    sf::RenderWindow* window;      // Pointer to the game window.
+    sf::Sprite loadscreen;         // Sprite for the loading screen.
+    sf::Texture dannyTexture;      // Texture for AI player's sprite.
+    sf::IntRect dannySprite;       // IntRect for AI player's sprite.
+    sf::Sprite sprite;             // General purpose sprite.
 
-    // Create the loading screen texture:
-    sf::Texture loadscreenTexture;
+    // Loading Screen
+    sf::Texture loadscreenTexture; // Texture for the loading screen.
 
-    // Create paddles
-    sf::RectangleShape leftPaddle;
-    sf::Texture leftPaddleTexture;
-    sf::RectangleShape rightPaddle;
-    sf::Texture rightPaddleTexture;
-    sf::RectangleShape middleLine;
+    // Paddle Objects
+    sf::RectangleShape leftPaddle;     // Player's left paddle.
+    sf::Texture leftPaddleTexture;     // Texture for player's left paddle.
+    sf::RectangleShape rightPaddle;    // AI player's right paddle.
+    sf::Texture rightPaddleTexture;    // Texture for AI player's right paddle.
+    sf::RectangleShape middleLine;     // Center line of the game.
 
-    // Create ball
-    sf::CircleShape ball;
-    sf::Texture balltexture;
+    // Ball Object
+    sf::CircleShape ball;             // Ball object.
+    sf::Texture balltexture;          // Texture for the ball.
 
-    // Set font
-    sf::Font font;
+    // Font and Text Objects
+    sf::Font font;                    // Font for text rendering.
+    sf::Text pauseMessage;            // Text for displaying pause message.
+    sf::Text scoreCard;               // Text for displaying the score.
 
-    // Create text
-    sf::Text pauseMessage;
-    sf::Text scoreCard;
+    // Running Events
+    sf::Event event;                  // Event object for handling events.
+    bool isPlaying;                   // Flag indicating if the game is currently playing.
 
-    //Running events
-    sf::Event event;
-    bool isPlaying;
+    // Game Points
+    int userPoint;                    // Player's current points.
+    int cpuPoint;                     // AI player's current points.
 
-    //Game points
-    int userPoint;
-    int cpuPoint;
-
+    // Private methods
     void initVariables();
     void initWindow();
     void initPaddles();
     void initBall();
     void initFonts();
-	void initMessages();
+    void initMessages();
     void countScore();
     void danny();
     void moveDanny();
     void checkDanny();
+
 public:
-	//Constructors / Destructors
-	Game();
+    // Constructors / Destructors
+    Game();
 
-	//Accessors
-	const bool running() const;
+    // Accessors
+    const bool running() const;
 
-    //Functions
+    // Functions
     void movePaddles();
     void checkCollisions();
     void pollEvents();
